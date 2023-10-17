b7ba45e3cfab3f621fa378f64beb4cbe1854acd2
Venu Reddy Yalapathireddy <venureddyyalapathireddy@venus-air.mynetworksettings.com>
2023-10-17 17:55:33 -0400

    hello

--- Hellowrld.sh
+++ Hellowrld.sh
@@ -2,3 +2,17 @@
 ##my sample 
 
 echo "hello world"
+
+echo $BASH
+echo $BASH_VERSION
+echo $HOME
+echo $PWD
+
+name=VENU
+echo my name is $name
+
+# don't defind the values by susing the numberst starting 
+
+Number=10 
+Echo printing the nuber '"+"' $Number
+
